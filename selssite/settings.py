c9e10241b9ea193ddf1e7e949c04e63123b00e76
이정근 <nooreong0503@gmail.com>
2023-09-04 13:00:27 +0900

    [ENH] modify settings.py url

--- selssite/settings.py
+++ selssite/settings.py
@@ -157,7 +157,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://www.sejongsels.kro.kr",
+    "http://sejongsels.kro.kr",
 
 ]
 
